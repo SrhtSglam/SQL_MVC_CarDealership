481407cb5eed6f4611df0dd45024651817d68da2
Serhat Sağlam <serhatsaglamb@gmail.com>
2024-10-02 09:32:46 +0300

    Create a project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SQL_MVC_CarDealership.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
